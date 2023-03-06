5cfb3b56d0c5e3ebdc124fb8e1a41a5ef7de2cf3
dimyG <dimgeows@gmail.com>
2023-03-06 16:59:12 +0200

    allow cors in prod too

--- auth_prj/settings/prod.py
+++ auth_prj/settings/prod.py
@@ -5,9 +5,9 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # if we need to allow calls from other domains we should include the corsheaders app
-INSTALLED_APPS.remove('corsheaders')
-MIDDLEWARE.remove('corsheaders.middleware.CorsMiddleware')
-# CORS_ALLOW_ALL_ORIGINS = True
+# INSTALLED_APPS.remove('corsheaders')
+# MIDDLEWARE.remove('corsheaders.middleware.CorsMiddleware')
+CORS_ALLOW_ALL_ORIGINS = True
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'  # todo: it does nothing, use a proper one
